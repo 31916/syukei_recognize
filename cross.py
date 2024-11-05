21cd16c781fc14ab27e100abba70a357570ea90c
31916 <135517180+31916@users.noreply.github.com.>
2024-11-06 03:37:03 +0900

    sub

--- cross.py
+++ cross.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 import os
+import random
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.models import Sequential
@@ -36,8 +37,14 @@ def main():
             with open(os.path.join(data_dir, file_name), 'r', encoding='utf-8') as f:
                 data = json.load(f)
 
+            # データが5つ未満の場合はそのまま使用
+            if len(data) > 5:
+                sampled_data = random.sample(data, 5)  # 5つのデータのみをランダムに抽出
+            else:
+                sampled_data = data  # 5つ未満ならそのまま使用
+
             # 右手と左手の角度を取得
-            for entry in data:
+            for entry in sampled_data:
                 right_hand_angles.append(entry['right_hand_angle'])
                 left_hand_angles.append(entry['left_hand_angle'])
                 labels.append(hand_shape_label)  # 手形番号をラベルとして追加
@@ -106,7 +113,7 @@ def main():
     # 結果をJSON形式で保存（outputディレクトリ内に保存）
     output_dir = './do/data/output'  # outputディレクトリを指定
     os.makedirs(output_dir, exist_ok=True)  # ディレクトリが存在しない場合は作成
-    output_path = os.path.join(output_dir, "crossValidationResults.json")
+    output_path = os.path.join(output_dir, "cross_Results.json")
 
     with open(output_path, "w", encoding='utf-8') as f:
         json.dump(results, f, indent=4, ensure_ascii=False)
